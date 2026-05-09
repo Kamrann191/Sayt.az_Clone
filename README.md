c9c8864099ef54c7502d674a992558a63dc24c3e
Kamrann191 <kamranquliyev2005@gmail.com>
2026-05-09 15:10:01 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sayt.az
+
+This project is configured for deployment to Ufazien Hosting.
+
+## Deploy
+
+Deploy your website to Ufazien:
+
+```bash
+ufazienjs deploy
+```
+
+Your website will be available at your configured subdomain.
---
+++ script/data.js
@@ -0,0 +1,352 @@
+const menuData = [
+    [
+        {
+            title: 'Saytların hazırlanması',
+            description: 'Veb saytların hazırlanması',
+            url: 'https://sayt.az/icons/website-program.svg'
+        },
+        {
+            title: 'Hökumət və qeyri-kommersiya saytları',
+            description: 'Hökumət və qeyri-kommersiya  saytları',
+            url: 'https://api.websiteca.com/storage/lJ9i3mHSrGbIyOFk6VnqpcyzeZgiKmjyiUfdEhAc.svg'
+        },
+        {
+            title: 'Kiçik biznes saytları',
+            description: 'Kiçik bizneslər  üçün saytları',
+            url: 'https://api.websiteca.com/storage/fcdQoEskESpjwrkLUcNKqvm0ZFwzn7kAspkBfwI5.svg'
+        },
+        {
+            title: 'E-ticarət saytları',
+            description: 'E-ticarət saytları',
+            url: 'https://api.websiteca.com/storage/vQyageZW9Eb4rKazAWwrY3xUTem15TN9Ofdk8oUk.svg'
+        },
+        {
+            title: 'Hüquq firma saytı',
+            description: ' Hüquq firma saytı',
+            url: 'https://api.websiteca.com/storage/otN2FW4K7vPTfJRqYOqYJMRj1VZDil02yYCmA0mx.svg'
+        },
+        {
+            title: 'Bloqlar və Şəxsi Vebsayt',
+            description: 'Bloqlar və Şəxsi Vebsayt',
+            url: 'https://api.websiteca.com/storage/zJjPDvohOxpgJj7wenVSBaGbTgNOuigFvjxLmhw5.svg'
+        },
+        {
+            title: 'Daşınmaz əmlak veb saytı',
+            description: 'Daşınmaz əmlak veb saytı',
+            url: 'https://api.websiteca.com/storage/RG2ZavDFVNsih7XSr36x2pxKPQxXekuAgO2VXif6.svg'
+        },
+
+        {
+            title: 'Tibb Doktor Saytlar',
+            description: 'Tibb Doktor Saytlar',
+            url: 'https://api.websiteca.com/storage/khwNG7uXgAPtW9wilTtK12LaUADpZgeQb3UvOqvB.svg'
+        },
+        {
+            title: 'WordPress Saytları',
+            description: 'WordPress Saytları',
+            url: 'https://api.websiteca.com/storage/qKRz4fKbiAGalPgCtcAGVuV5AnCAhx8WkRAMdV3L.svg'
+        },
+    ],
+    [
+        {
+            title: 'Domen',
+            description: 'Etibarlı xidmət',
+            url: 'https://sayt.az/icons/network-browser.svg'
+        },
+        {
+            title: 'Pulsuz Domen Qeydiyyatı',
+            description: 'Sürətli qeydiyyat',
+            url: 'https://sayt.az/icons/network-browser.svg'
+        }
+    ],
+    [
+        {
+            title: 'Veb Hostinq',
+            description: '₼10.00/ay-dan başlayaraq',
+            url: 'https://sayt.az/icons/hosting-icon.svg'
+        },
+        {
+            title: 'VPS',
+            description: '15.00/ay-dan başlayaraq',
+            url: 'https://sayt.az/icons/vps-icon.svg'
+        },
+        {
+            title: 'Server',
+            description: '99.00/ay-dan başlayaraq',
+            url: 'https://sayt.az/icons/server-icon.svg'
+        }
+    ],
+    [
+        {
+            title: 'Vebsayt sifarişinizi izləyin',
+            description: 'Sifarişinizi asanlıqla izləyin',
+            url: 'https://sayt.az/icons/order-track.svg'
+        },
+        {
+            title: 'Sayt Kalkulyatoru',
+            description: 'Saytın Qiymətini Öyrən',
+            url: 'https://sayt.az/icons/calculator.svg'
+        },
+        {
+            title: 'SEO Xidməti',
+            description: 'Axtarış Sistemi Optimizasiyası',
+            url: 'https://api.websiteca.com/storage/sgUxupnExZlBR6tNePlJ6L569Cp65wKFojRZyl0F.svg'
+        },
+        {
+            title: 'Mobil tətbiq xidməti:',
+            description: 'Mobil tətbiq xidməti',
+            url: 'https://api.websiteca.com/storage/T54KkNvzpw6VEXUE3YoGrhr1NhpHKmFmCFx9wIwh.svg'
+        },
+        {
+            title: 'Texniki Xidmət',
+            description: 'Texniki Xidmət',
+            url: 'https://api.websiteca.com/storage/IbI930H5VWeP3FFJXyJL2DtPWF1gy9ASREm90EBs.svg'
+        },
+        {
+            title: 'Sosial Media Marketinq',
+            description: 'Sosial Media Marketinq',
+            url: 'https://api.websiteca.com/storage/MWCG5ptX87GTmLKVrxhewNDRblz5IxF0ZWRoGKWn.svg'
+        }
+    ],
+    [
+        {
+            title: 'Haqqımızda',
+            description: 'Bizim komandaüüzvlərimiz',
+            url: 'https://sayt.az/icons/about-icon.svg'
+        }, {
+            title: 'Bloq',
+            description: 'Ən son xəbərlər və faydalı məlumatlar',
+            url: 'https://sayt.az/icons/blog-icon.svg'
+        }, {
+            title: 'Bütün StartUplar',
+            description: 'Startupları idarə edən komandamıza qoşulun',
+            url: 'https://sayt.az/icons/cpu.svg'
+        }, {
+            title: 'Portfolio',
+            description: 'Hazırladığımız hər bir iş bizim üçün dəyərlidir',
+            url: 'https://sayt.az/icons/portfolio-icon.svg'
+        }, {
+            title: 'Karyera',
+            description: 'Hazırkı vakansiyalarımız',
+            url: 'https://sayt.az/icons/career-icon.svg'
+        }, {
+            title: 'Freelance',
+            description: 'Sizdə qeydiyyatdan keçməklə freelance işləyə bilərsiz',
+            url: 'https://sayt.az/icons/note-icon.svg'
+        }, {
+            title: 'Əlaqə Forması',
+            description: 'Əlaqə Saxlayın',
+            url: 'https://sayt.az/icons/contact-icon.svg'
+        },
+    ]
+
+]
+const islerimiz = [
+    {
+        ad: 'BIOPHYSICS.AZ',
+        url: 'https://api.websiteca.com/storage/wKyykqEbaB5lQ5Q4bq6r5KcpP91bZytQXDtBopDY.png'
+    },
+    {
+        ad: 'UREB.COM',
+        url: 'https://api.websiteca.com/storage/Mupn6xSf0FTlbBj0ilDZAqXuLiLrwFGjZct4gQLH.png'
+    },
+    {
+        ad: 'UG.NEWS',
+        url: 'https://api.websiteca.com/storage/ZYfooZQ12g9cwbdUYdc9wEsHDXjshxZmRmQB5z9h.png'
+    },
+    {
+        ad: 'FOOTBALL.BIZ',
+        url: 'https://api.websiteca.com/storage/2MQEImHKxKRLKbEGPUt9xQY2QiSlQw1gWE7RdkKm.png'
+    },
+    {
+        ad: 'WISHER.AZ',
+        url: 'https://api.websiteca.com/storage/mMkkjJ32v64o4mN5tuMBvn7SWWkd1dPBBM1ZBzE2.png'
+    },
+    {
+        ad: 'IMEXCOMMODITIES.COM',
+        url: 'https://api.websiteca.com/storage/7ZV1bRucrPWExvpsJqLyFiELk7fHAOk6ZtPVHpNl.png'
+    },
+    {
+        ad: 'AZFENTTQ.AZ',
+        url: 'https://api.websiteca.com/storage/l7CnTnqSgXj1XA2ZMFtVvo4E8j2nZ4wgElrDehh7.png'
+    },
+    {
+        ad: 'NOTEHUB.AZ',
+        url: 'https://api.websiteca.com/storage/vjciI2KyS5K2wrvpm1JlLFKdcd7WlG4wB0GTlItY.png'
+    }
+    ,
+    {
+        ad: 'AZEHOLIDAYS.AZ',
+        url: 'https://api.websiteca.com/storage/7g8cGPiEFNN7KfmRLC7Hes27A0xj9UD85yxLjSXZ.png'
+    },
+    {
+        ad: 'VIPCONSULTING.AZ',
+        url: 'https://api.websiteca.com/storage/J0npE6PgAdvfSUmaUYdqdbQHplamTsx4RiOGB7wJ.png'
+    },
+    {
+        ad: 'ZEMED.AZ',
+        url: 'https://api.websiteca.com/storage/2Nr06VCFo72kSNXyGBNM6Vp8sl95xifE8PDKUnXO.png'
+    }, {
+        ad: "ESCO.AZ",
+        url: "https://api.websiteca.com/storage/STK6FPMjjvSWhL030mPfPDDzeUFvpoAEKVSYfgkw.png"
+    },
+    {
+        ad: 'CASPILIMO.COM',
+        url: 'https://api.websiteca.com/storage/oHGuQndVvObtojo1JtE49ALXsbAcUSxsj5M56Qf3.png'
+    },
+    {
+        ad: 'BAYMAK.AZ',
+        url: 'https://api.websiteca.com/storage/oHGuQndVvObtojo1JtE49ALXsbAcUSxsj5M56Qf3.png'
+    }, {
+        ad: 'SELJANNOVRUZLU.COM',
+        url: "https://api.websiteca.com/storage/tKp5LvACzbuA7RTDYVhRO3Jvq60x2JZjzq7lYVZK.png"
+    },
+    {
+        ad: "QARMON.AZ",
+        url: "https://api.websiteca.com/storage/F1dmQj4FgeeUXLxfRPSBI8ftmELJuCtsWCF1kHOj.png"
+    },
+    {
+        ad: 'NAFTALANSANTORIYA.COM',
+        url: 'https://api.websiteca.com/storage/jZI68lSJ4mkBfuzPd5jD5hOvcbweYv8046gfQO9f.png'
+    },
+    {
+        ad: 'DOCFARHAD.AZ',
+        url: 'https://api.websiteca.com/storage/gcY5xpQUdl0l1w08pdcojI3lYKIOpbRsTA7I59SP.png'
+    },
+    {
+        ad: 'HEDEFINSAAT.COM',
+        url: 'https://api.websiteca.com/storage/XLMD3wcv76aEMYFbbIL5BOLkRvYd6eTEoTlxLuRf.png'
+    }, {
+        ad: 'BAHMANAHMADLI.COM',
+        url: 'https://api.websiteca.com/storage/CKurYrJocHfOBOpPf7b4w2or4B8Lr69ucbhoJ0m1.jpg'
+    }, {
+        ad: 'TOVUZAQROKOMPLEKS.COM',
+        url: 'https://api.websiteca.com/storage/JWXew0F20ev6NXLLuMDwPNx91zMjfEBhACv6gK72.jpg'
+    }, {
+        ad: 'METBUAT.AZ',
+        url: 'https://api.websiteca.com/storage/M7iPoAGuMxIuMXKwSt4ZHOeTIHQbZzBE83IfJvEL.jpg'
+    }, {
+        ad: 'AGROKIMYA.AZ',
+        url: 'https://api.websiteca.com/storage/rRkpiwP29e15E0VH8yAmEtumTxdND6LPkTgzLgs4.jpg'
+    },
+    {
+        ad: 'AZERBAIJANJOURNALISTS.AZ',
+        url: 'https://api.websiteca.com/storage/DlosbjYnNSzN23aGOLLzuUrizgG4ZwgvP1saffZ0.jpg'
+    },
+    {
+        ad: 'PLANNING.AZ',
+        url: 'https://api.websiteca.com/storage/o76exUgx9DfL8dP2VrSCofJkYLRBf3p79l09mnBh.jpg'
+    },
+    {
+        ad: 'WWW.ART-TRIO.AZ',
+        url: 'https://api.websiteca.com/storage/pGOaLJc2bIgxb32dnk3Eoi9VbhyjQjKx97BBgJJO.jpg'
+    }
+]
+const pricesData = {
+    "Veb sayt əlavələri": [
+        {
+            "title": "Bloq",
+            "price": 200
+        },
+        {
+            "title": "Üzv girişi / Profillər",
+            "price": 400
+        },
+        {
+            "title": "Responsive",
+            "price": 100
+        },
+        {
+            "title": "Forum",
+            "price": 400
+        },
+        {
+            "title": "Mesajlaşma",
+            "price": 50
+        },
+        {
+            "title": "Canlı Söhbət",
+            "price": 50
+        },
+        {
+            "title": "Qalereya",
+            "price": 100
+        },
+        {
+            "title": "Əlaqə Forması",
+            "price": 100
+        },
+        {
+            "title": "Saytda Axtarış",
+            "price": 100
+        },
+        {
+            "title": "Xəritə Məlumatı / Geolocation",
+            "price": 100
+        },
+        {
+            "title": "Rezervasyon",
+            "price": 400
+        },
+        {
+            "title": "Sosial Media İnteqrasiyası",
+            "price": 50
+        },
+        {
+            "title": "Analitika / İzləmə",
+            "price": 50
+        },
+        {
+            "title": "Admin Panel",
+            "price": 300
+        }
+    ],
+    "Axtarış motorunun optimallaşdırılması": [
+        {
+            "title": "Başlıqlar & Meta",
+            "price": 200
+        },
+        {
+            "title": "Açar söz",
+            "price": 300
+        },
+        {
+            "title": "+20 Qabaqcıl Texnika",
+            "price": 300
+        },
+        {
+            "title": "Başlıq Teqləri",
+            "price": 100
+        },
+        {
+            "title": "Sitemap",
+            "price": 100
+        }
+    ],
+    "Elektron ticarət əlavələri": [
+        {
+            "title": "Ödəniş",
+            "price": 200
+        },
+        {
+            "title": "Səbət",
+            "price": 500
+        },
+        {
+            "title": "Məhsullar",
+            "price": 300
+        },
+        {
+            "title": "Bəyəndiklərim",
+            "price": 300
+        },
+        {
+            "title": "Filtr",
+            "price": 300
+        },
+        {
+            "title": "Məhsul Haqqında",
+            "price": 200
+        }
+    ]
+}
